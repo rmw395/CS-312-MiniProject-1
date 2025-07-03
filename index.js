67b1857cb3963d124636673bab4c4cc294da43a2
risa walles <rmw395@nau.edu>
2025-07-03 11:32:18 -0700

    more actual ejs stuff (untested yet)

--- index.js
+++ index.js
@@ -3,25 +3,58 @@ import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
-app.use(express.urlencoded({ extended: true}));
-app.use(morgan("tiny"));
+let posts = [];
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}.`)
 });
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {posts});
+});
+
+app.get("/create", (req, res) => {
+    res.render("create.ejs");
+});
+
+app.get("/edit/:id", (req, res) => {
+    // ah
 });
 
 app.post("/submit", (req, res) => {
-    const post = req.form;
-    res.render("index.ejs", {
-        newPost: post,
-      });
+    const postAuthor = req.form['author'];
+    const postTitle = req.form['title'];
+    const postContent = req.form['content'];
+    const newPost = {
+        pAuthor: postAuthor,
+        pTitle: postTitle,
+        pContent: postContent,
+        // pDate: today.toDateString()
+    };
+
+    posts.push(newPost);
+    res.redirect("/");
+});
+
+app.post("/edit/:id", (req, res) => {
+    const postId = req.params.id;
+
+    posts[postId].pAuthor = req.form.author;
+    posts[postId].pTitle = req.form.title;
+    posts[postId].pContent = req.form.content;
+
+    res.redirect("/");
+});
+
+app.post("/delete/:id", (req, res) => {
+    // ahhh
 });
